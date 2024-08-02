a846043adf3a67643ed0d3c15d893e46aada07ee
vishwamohini15 <vishwamohini0015@gmail.com>
2024-08-02 13:06:37 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,77 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const[title, settitle]=useState("")
+  const[discription, setdiscription]=useState("")
+  const [maintask, setmaintask] = useState([])
+
+function submithandler(e){
+  e.preventDefault()
+  
+  setmaintask([...maintask, {title,discription}])
+  console.log(maintask);
+  console.log("fgtre");
+  console.log(title);
+  console.log(discription);
+  setdiscription("")
+  settitle("")
+}
+
+const deletehandler=(i)=>{
+let copytask=[...maintask]
+copytask.splice(i,1)
+setmaintask(copytask)
+}
+
+let renderTask=<h2>NO Task Available</h2>
+if (maintask.length>0) {
+  renderTask=maintask.map((t,i)=>{
+    return (
+      <li  key={i} className='boxhuu'>
+    <div  className='tastbox'>
+      <h3>{t.title}</h3>
+      <h5>{t.discription}</h5>
     </div>
+    <button  onClick={deletehandler} className='dltbtn'>Delete</button>
+    </li>
+    );
+    })
+}
+
+  return (
+<>
+<div  className='design'>
+<h1>TO-DO-List</h1>
+<form  onSubmit={submithandler}>
+  <input type='text' placeholder='Enter your task' 
+  value={title}
+  onChange={(e)=>{
+// console.log(e.target.value);
+settitle(e.target.value)
+  }}
+  ></input>
+
+  <input type='text' placeholder='Enter your description'
+   value={discription}
+   onChange={(e)=>{
+ // console.log(e.target.value);
+ setdiscription(e.target.value)
+   }}
+  ></input>
+
+  <button className='btn'>button</button>
+
+</form>
+<hr></hr>
+<div className='container'>
+  <ul>
+    {renderTask}
+  </ul>
+</div>
+
+</div>
+</>
   );
 }
 
